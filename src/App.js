ec8d21764aa5a91a142156e04758ced435f783b7
redjon.mata <redjon.mata@infotelecom.al>
2020-06-05 14:50:21 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,66 +1,183 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import CreatableSelect from 'react-select/creatable';
+/* eslint-disable */
 
-class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [], text: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  render() {
-    return (
-        <div>
-          <h3>TODO</h3>
-          <TodoList items={this.state.items} />
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="new-todo">
-              What needs to be done?
-            </label>
-            <input
-                id="new-todo"
-                onChange={this.handleChange}
-                value={this.state.text}
-            />
-            <button>
-              Add #{this.state.items.length + 1}
-            </button>
-          </form>
-        </div>
-    );
-  }
-
-  handleChange(e) {
-    this.setState({ text: e.target.value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    if (this.state.text.length === 0) {
-      return;
+class App extends Component {
+    render() {
+        return (
+            <SimpleForm/>
+        );
     }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
-  }
 }
 
-class TodoList extends React.Component {
-  render() {
-    return (
-        <ul>
-          {this.props.items.map(item => (
-              <li key={item.id}>{item.text}</li>
-          ))}
-        </ul>
+const validEmailRegex = RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+const validFirstAndLastNameRegex = RegExp(/^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/);
+const validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach(
+        (val) => val.length > 0 && (valid = false)
     );
-  }
+    return valid;
 }
 
-export default TodoApp;
+const countErrors = (errors) => {
+    let count = 0;
+    Object.values(errors).forEach(
+        (val) => val.length > 0 && (count = count + 1)
+    );
+    return count;
+}
+
+class SimpleForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            hobbies: [
+                { value: 'chocolate', label: 'Chocolate' },
+                { value: 'strawberry', label: 'Strawberry' },
+                { value: 'vanilla', label: 'Vanilla' },
+                { value: 'banana', label: 'Banana' },
+                { value: 'cookie', label: 'Cookie' },
+            ],
+            formValid: false,
+            errorCount: null,
+            errors: {
+                firstName: '',
+                lastName: '',
+                email: '',
+            },
+            selectedHobbies: [],
+        };
+    }
+
+    handleChange = (event) => {
+        event.preventDefault();
+        const {name, value} = event.target;
+        let errors = this.state.errors;
+
+        switch (name) {
+            case 'firstName':
+                errors.firstName =
+                    validFirstAndLastNameRegex.test(value)
+                        ? ''
+                        : 'First Name must only contain letters and spaces!';
+                break;
+            case 'lastName':
+                errors.lastName =
+                    validFirstAndLastNameRegex.test(value)
+                        ? ''
+                        : 'Last Name must only contain letters and spaces!';
+                break;
+            case 'email':
+                errors.email =
+                    validEmailRegex.test(value)
+                        ? ''
+                        : 'Email is not valid!';
+                break;
+            default:
+                break;
+        }
+
+        this.setState({
+            errors, [name]: value,
+        });
+    }
+
+    handleHobbiesChange = (newValue) => {
+        this.setState({selectedHobbies: newValue})
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({formValid: validateForm(this.state.errors)});
+        this.setState({errorCount: countErrors(this.state.errors)});
+    }
+
+    render() {
+        const {firstName, lastName, email, errors, selectedHobbies, formValid} = this.state;
+        return (
+            <div className={`row`}>
+                <div className={`col-md-3 wrapper-right`}>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className='firstName'>
+                            <label htmlFor="firstName">First Name</label>
+                            <input className={`form-control `} type='text' name='firstName' defaultValue={this.state.firstName} onChange={this.handleChange}/>
+                            {errors.firstName.length > 0 &&
+                            <span className={`alert-danger`}>{errors.firstName}</span>}
+                        </div>
+                        <div className='lastName'>
+                            <label htmlFor="lastName">Last Name</label>
+                            <input className={`form-control `} type='text' name='lastName' defaultValue={this.state.lastName} onChange={this.handleChange}/>
+                            {errors.lastName.length > 0 &&
+                            <span className={`alert-danger`}>{errors.lastName}</span>}
+                        </div>
+                        <div className='email'>
+                            <label htmlFor="email">Email</label>
+                            <input className={`form-control `} type='email' name='email' defaultValue={this.state.email} onChange={this.handleChange}/>
+                            {errors.email.length > 0 &&
+                            <span className={`alert-danger`}>{errors.email}</span>}
+                        </div>
+                        <div className={`hobbies`}>
+                            <label htmlFor="hobbies">Hobbies</label>
+                            <CreatableSelect
+                                isMulti
+                                options={this.state.hobbies}
+                                onChange={this.handleHobbiesChange}
+                            />
+                            <span className={`alert-danger`}>{errors.password}</span>
+                        </div>
+                        <button className={`btn btn-primary`}>Create</button>
+                    </form>
+                </div>
+                <div className={`col-md-3 wrapper-left`}>
+                    <div className={`vl`}>
+                        <div className={`results`}>
+                            {
+                                this.state.errorCount !== null
+                                    ?
+                                    <p>
+                                        {
+                                            formValid
+                                            ?
+                                                firstName.length > 0 && lastName.length > 0 && email.length > 0 && this.state.selectedHobbies.length > 0
+                                                ?
+                                                    <div>
+                                                        <p>First Name: {firstName}</p>
+                                                        <p>Last Name: {lastName}</p>
+                                                        <p>Email: {email}</p>
+                                                        <p>Selected hobbies:</p>
+                                                        <SelectedHobbies hobbies={selectedHobbies} />
+                                                    </div>
+                                                : 'One or more fields are empty.'
+
+                                            : 'Submit is invalid. Check your errors.'
+                                        }
+                                    </p>
+                                : 'Form not submitted.'
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class SelectedHobbies extends React.Component {
+    render() {
+        return (
+            <ul>
+                {this.props.hobbies.map(item => (
+                    <li>{item.label}</li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+export default App;
